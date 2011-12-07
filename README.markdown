4dcb696b318dddbccbe7fb841f4f4aac48bc08aa
Suhaib Fahmy <sfahmy@gmail.com>
2011-12-07 11:53:45 +0800

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,14 @@
+=About=
+This respository will hold a series of tools for procesing Verilog files, all written in Python.
+
+==`vmodule.py`==
+Usage:
+`./vmodule.py filename.v`
+
+This script extracts the module declaration from the Verilog file, outputs the modules name, and a list of input and output ports (and inout ports if present), each item being a tuple of port name, port width, most significant bit index, least significant bit index.
+
+Known Limitations:
+
+- The script will not process files with more than one module
+- It only understands Verilog-2001 module declarations (with the directions and widths within the brackets)
+
---
+++ vmodule.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+# Load file argument contents into a single string
+f = open(sys.argv[1])
+contents = f.read()
+f.close()
+
+# Strip C-style comments, taken from http://stackoverflow.com/a/241506
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return ""
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
+
+nocomment = comment_remover(contents)
+
+# Extract module declaration as section between module keyword and semicolon
+moduledec = nocomment.split('module')[1].split(';')[0]
+
+# Module name is prior to bracket
+module_name = moduledec.split('(')[0].strip()
+print 'Module Name: ' + module_name
+
+# Module ports are section within brackets, separated by comma
+# Remove whitespace and strip reg from output declaration
+module_ports = moduledec.split('(')[1].split(')')[0].split(',')
+ports = [a.strip().replace(' reg ',' ').replace(' wire ',' ') for a in module_ports]
+# print ports
+
+# Remove superfluous spaces in port index
+ports_a = [re.sub(r'\[\s*(\d+)\s*:\s*?(\d+)\s*\]', r'[\1:\2]', a) for a in ports]
+
+# Ensure a space after square brackets for subsequent split
+ports_b = [re.sub(r'\]([a-zA-Z])', r'] \1', b) for b in ports_a]
+
+# Construct a list of tuples for input and output ports
+# Each tuple is the port name, width, MSB index, LSB index
+in_ports = []
+out_ports= []
+inout_ports= []
+for a in ports_b:
+	a_split= a.split()
+	# print a_split
+	if len(a_split) == 3:
+		# Extract two numbers separated by colon surrounded by square brackets
+		bits = [int(x) for x in a_split[1].split('[')[1].split(']')[0].split(':')]
+		bit_width = abs(bits[0]-bits[1])+1
+	else:
+		bit_width = 1
+		bits = [0,0]
+	
+	port_tuple = (a_split[-1], bit_width, bits[0], bits[1])
+
+	if a_split[0] == 'input':
+		in_ports.append(port_tuple)
+	elif a_split[0] == 'output':
+		out_ports.append(port_tuple)
+	elif a_split[0] == 'inout':
+		inout_ports.append(port_tuple)
+
+print 'Inputs (name, width, msb, lsb):\n' + str(in_ports)
+print 'Outputs: (name, width, msb, lsb):\n' + str(out_ports)
+if len(inout_ports) > 0:
+	print 'InOuts: (name, width, msb, lsb):\n' + str(inout_ports)
